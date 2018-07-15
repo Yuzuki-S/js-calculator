7e32b8877d449d366812cba27fb90e5ae0cab0ab
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-07-15 18:06:17 +1200

    completed calculate function

--- js-calculator/calculator.js
+++ js-calculator/calculator.js
@@ -12,37 +12,54 @@ function createEventListeners(){
 let entries = [];
 let temp = ''
 function onClickCalculate(){
-  
-  console.log("Start temp " + temp)
-  console.log("Start entries " + entries)
   let value = this.innerHTML;
-  display[0].innerHTML = value;
+ 
   
   if(value === "AC"){
+    display[0].innerHTML = "";
     entries=[];
     temp = '';
   } else if(value === "CE") {
+    display[0].innerHTML = "";
     temp = ''
   } else if (value === "="){
     calculateAnswer()
   } else {
     if (!isNaN(value) || value === '.') {
+      display[0].innerHTML = display[0].innerHTML + value;
       temp += value
     } else {
+      display[0].innerHTML = ""
       entries.push(temp);
       entries.push(value);
       temp = '';
     }
   }
 
-  console.log(temp)
-  
 }
 
 function calculateAnswer(){
   entries.push(temp);
-  display[0].innerHTML = "hello"
-  console.log(display)
-  console.log(entries)
-  
+  var total = Number(entries[0]);
+  for (var i = 1; i < entries.length; i++) {
+    var nextNum = Number(entries[i+1])
+    var symbol = entries[i];
+    
+    if (symbol === '+') { 
+      total += nextNum; 
+    } else if (symbol === '-') { 
+      total -= nextNum; 
+    } else if (symbol === '*') { 
+      total *= nextNum; 
+    } else if (symbol === '/') {
+       total /= nextNum; 
+    }
+    
+    i++;
+    
+  } 
+  display[0].innerHTML = total
+  temp = ""
+  entries = []
+  total = 0
 }
